1e5beac7ce5a7f87c15f72120767f0910109d018
bintangx1902 <60544749+bintangx1902@users.noreply.github.com>
2021-10-25 23:29:59 +0700

    Update Views

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,7 @@ from django.views.generic import *
 from django.contrib.auth.models import User
 from .forms import *
 from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 
 class DashboardView(TemplateView):
@@ -60,3 +61,44 @@ def presence(request, link, pk):
             this_pre.presence.add(request.user)
 
     return redirect('')
+
+
+class ScanQRCode(UpdateView):
+    model = QRCodeGenerator
+    template_name = ''
+    form_class = None
+    query_pk_and_slug = True
+    pk_url_kwarg = 'pk'
+
+    def get_qr(self):
+        return get_object_or_404(QRCodeGenerator, pk=self.kwargs['pk'])
+
+    def form_valid(self, form):
+        get_qr = self.get_qr()
+        user = self.request.user
+        the_code = self.request.POST.get('the_code')
+        if the_code and the_code == get_qr.qr_code:
+            form.instance.presence = user.pk
+        return super(ScanQRCode, self).form_valid(form)
+
+    def dispatch(self, request, *args, **kwargs):
+        get_qr = self.get_qr()
+        user = self.request.user
+        is_presence = get_qr.presence.filter(id=user.pk)
+        if is_presence.exists():
+            return HttpResponseRedirect(reverse(''))
+        return super(ScanQRCode, self).dispatch(request, *args, **kwargs)
+
+
+def scan(request, pk):
+    get_qr = get_object_or_404(QRCodeGenerator, pk=pk)
+    user = request.user
+    is_presence = get_qr.presence.filter(id=user.pk)
+    if request.method == 'POST':
+        if is_presence.exists():
+            return redirect('')
+        the_code = request.POST.get('the_code')
+        if the_code and the_code == get_qr.qr_code:
+            get_qr.presence.add(user)
+
+    return
