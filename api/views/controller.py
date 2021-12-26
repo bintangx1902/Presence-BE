456aafce4315ee216c955a62d5920cdf12901060
bintangx1902 <bintangpratomoprawirodirjo>
2021-12-26 17:06:32 +0700

    Update API @ agency

--- api/views/controller.py
+++ api/views/controller.py
@@ -57,11 +57,10 @@ class QRCodeForPresence(APIView):
             else:
                 break
 
-        create = QRCodeGenerator.objects.create(
+        QRCodeGenerator.objects.create(
             qr_code=code, valid_until=valid_until,
             agency=user.user.agency, creator=user
         )
-        create.save()
         qr = QRCodeGenerator.objects.filter(agency=user.user.agency, valid_until__gte=timezone.now())
         serializer = QRCodeSerializer(qr, many=True)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -80,7 +79,33 @@ class InvitationLinkEndPoint(APIView):
         return Response(serializer.data)
 
     def post(self, format=None):
+        token = self.request.COOKIES.get('jwt')
+        if not token:
+            raise AuthenticationFailed("There is no Agency Code! ", status.HTTP_403_FORBIDDEN)
         unique = self.request.GET.get('unique')
         if not unique:
-            raise AuthenticationFailed("There is no Agency Code! ", status.HTTP_403_FORBIDDEN)
-        return
+            raise AuthenticationFailed("Agency code not detected!", status.HTTP_403_FORBIDDEN)
+        agency = AgencyName.objects.filter(unique_code=unique)
+        if not agency:
+            raise AuthenticationFailed("There is no such Agency! ", status.HTTP_403_FORBIDDEN)
+
+        payload = payloads(token)
+        user = this_user(payload)
+        code = generate_invitation_code()
+        inv = InvitationLink.objects.all()
+        data = [x.link for x in inv]
+        valid_until = timezone.now() + datetime.timedelta(.5)
+        while True:
+            if code in data:
+                code = generate_qr_code()
+            else:
+                break
+
+        InvitationLink.objects.create(
+            link=code, valid_until=valid_until,
+            agency=user.user.agency, invitee=user
+        )
+
+        inv = inv.filter(agency=user.user.agency)
+        serializer = InvitationLinkSerializer(inv, many=True)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
